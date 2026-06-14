19121cfbbdd453808f1aa71a3f3fce99a5971d77
Owen <owencasabuena@gmail.com>
2026-06-14 09:42:27 +0800

    feat: implement drag-and-drop ship placement and game state lock

--- src/index.js
+++ src/index.js
@@ -11,6 +11,25 @@ const fleetBlueprint = [
     { name: 'Patrol Boat', length: 2 }
 ];
 
+const dockyard = document.querySelector('.dockyard');
+fleetBlueprint.forEach(shipData => {
+    const ship = document.createElement('div');
+    ship.classList.add('ship-drag');
+    ship.setAttribute('draggable', 'true');
+    ship.dataset.length = shipData.length;
+    ship.dataset.name = shipData.name;
+
+    dockyard.appendChild(ship);
+});
+
+const ships = document.querySelectorAll('.ship-drag');
+ships.forEach(ship => {
+    ship.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('ship-length', event.target.dataset.length);
+        event.dataTransfer.setData('ship-name', event.target.dataset.name);
+    });
+});
+
 let player1;
 let player2;
 let playerBoardDiv;
@@ -24,17 +43,36 @@ function initGame() {
         const p1Ship = new Ship(shipData.name, shipData.length);
         const p2Ship = new Ship(shipData.name, shipData.length);
 
-        player1.gameboard.placeShipRandomly(p1Ship);
         player2.gameboard.placeShipRandomly(p2Ship);
     });
 
     playerBoardDiv = document.querySelector('.player-board');
+    playerBoardDiv.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    });
+
+    playerBoardDiv.addEventListener('drop', (event) => {
+        if(!event.target.classList.contains('cell')) return;
+
+        const shipLength = parseInt(event.dataTransfer.getData('ship-length'));
+        const shipName = event.dataTransfer.getData('ship-name');
+        const shipCoordinates = event.target.dataset.coordinate.split(',').map(Number);
+
+        const ship = new Ship(shipName, shipLength);
+        player1.gameboard.placeShip(ship, shipCoordinates);
+        playerBoardDiv.innerHTML = '';
+        createBoard(playerBoardDiv, player1.gameboard, true);
+        const selectedShip = document.querySelector(`[data-name="${shipName}"]`);
+        selectedShip.remove();
+
+        if(dockyard.children.length === 0) {
+            enemyBoardDiv.addEventListener('click', handleBoardClick);
+        }
+    });
     enemyBoardDiv = document.querySelector('.enemy-board');
 
     createBoard(playerBoardDiv, player1.gameboard, true);
     createBoard(enemyBoardDiv, player2.gameboard, false);
-
-    enemyBoardDiv.addEventListener('click', handleBoardClick);
 }
 
 function handleBoardClick(event) {
@@ -54,7 +92,7 @@ function playRound(targetCoordinate) {
         alert('You won!');
         return;
     }
-   
+
     let generateMove = player2.makeMove();
     let computerAttack = player1.gameboard.receiveAttack(generateMove);
     if (computerAttack) {
